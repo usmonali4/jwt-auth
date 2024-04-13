e231eefceb467be23ebee163fa6c5b2a2ea71849
Usmon <usmonpahlavonov@gmail.com>
2024-04-13 10:52:47 +0900

    some bug fix

--- src/main/java/com/levelUp/chatBox/Configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/levelUp/chatBox/Configuration/JwtAuthenticationFilter.java
@@ -37,7 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
             userEmail = jwtService.extractUsername(jwt);
             if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
                 UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-                if(jwtService.isTokenValid(userEmail, userDetails)){
+                if(jwtService.isTokenValid(jwt, userDetails)){
                     UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
